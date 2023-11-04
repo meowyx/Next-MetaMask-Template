2a97b7d80742dfa4a0e01b7b144b2b5bb30f9a2c
meowyx <qafka256@gmail.com>
2023-11-05 03:05:02 +0600

    all commit

---
+++ components/ui/NavBar.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import Link from "next/link";
+
+import WalletIcon from "../../public/icons/WalletIcon";
+
+import { Button } from "./ui/button";
+
+import { useState } from "react";
+import { useSDK, MetaMaskProvider } from "@metamask/sdk-react";
+import { formatAddress } from "../../lib/utils";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
+
+export const ConnectWalletButton = () => {
+  const [account, setAccount] = useState<string | undefined>();
+
+  const { sdk, connected, connecting } = useSDK();
+
+  const connect = async () => {
+    try {
+      const metaMaskAccount = (await sdk?.connect()) as string[];
+      setAccount(metaMaskAccount[0]);
+    } catch (err) {
+      console.warn(`No accounts found`, err);
+    }
+  };
+
+  const disconnect = () => {
+    if (sdk) {
+      sdk.terminate();
+      setAccount(undefined); // Optionally reset the account state
+    }
+  };
+
+  return (
+    <div className="relative">
+      {connected ? (
+        <Popover>
+          <PopoverTrigger>
+            <Button>{formatAddress(account)}</Button>
+          </PopoverTrigger>
+          <PopoverContent className="mt-2 w-44 bg-gray-100 border rounded-md shadow-lg right-0 z-10 top-10">
+            <button
+              onClick={disconnect}
+              className="block w-full pl-2 pr-4 py-2 text-left text-[#F05252] hover:bg-gray-200"
+            >
+              Disconnect
+            </button>
+          </PopoverContent>
+        </Popover>
+      ) : (
+        <Button disabled={connecting} onClick={connect}>
+          <WalletIcon className="mr-2 h-4 w-4" /> Connect Wallet
+        </Button>
+      )}
+    </div>
+  );
+};
+
+export const NavBar = () => {
+  const host =
+    typeof window !== "undefined" ? window.location.host : "defaultHost";
+
+  const sdkOptions = {
+    logging: { developerMode: false },
+    checkInstallationImmediately: false,
+    dappMetadata: {
+      name: "Next-Metamask-Boilerplate",
+      url: host, // using the host constant defined above
+    },
+  };
+
+  return (
+    <nav className="flex items-center justify-between max-w-screen-xl px-6 mx-auto py-7 rounded-xl">
+      <Link href="/" className="flex gap-1 px-6">
+        <span className="hidden text-2xl font-bold sm:block">
+          <span className="text-gray-900">Template</span>
+        </span>
+      </Link>
+      <div className="flex gap-4 px-6">
+        <MetaMaskProvider debug={false} sdkOptions={sdkOptions}>
+          <ConnectWalletButton />
+        </MetaMaskProvider>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ lib/utils.ts
@@ -0,0 +1,20 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const formatBalance = (rawBalance: string) => {
+  const balance = (parseInt(rawBalance) / 1000000000000000000).toFixed(2);
+  return balance;
+};
+
+export const formatChainAsNum = (chainIdHex: string) => {
+  const chainIdNum = parseInt(chainIdHex);
+  return chainIdNum;
+};
+
+export const formatAddress = (addr: string | undefined) => {
+  return `${addr?.substring(0, 8)}...`;
+};
